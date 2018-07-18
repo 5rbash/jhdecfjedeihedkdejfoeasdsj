641fcf0c61083aed7b8e021ecb55feb1eff004e4
5rbash <41377783+5rbash@users.noreply.github.com>
2018-07-18 17:41:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,34 +1,3 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '.'
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
-
-
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
@@ -222,9 +191,4 @@ if (message.content.startsWith("$ban")) {
   
 };
 });
-client.login("NDY4OTc4NTUxNzEwODEwMTEy.DjCa_Q.dvqOZsZxab7ztE2h71vRMqb_IBM");
-
-
-
-
 client.login(process.env.BOT_TOKEN);
