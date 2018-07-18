c260e51727d705ef2660780b4aa0246f9fe286b8
5rbash <41377783+5rbash@users.noreply.github.com>
2018-07-18 20:51:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '.'
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
 client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
   client.user.setGame(`on 15 servers | help$ | 5rbash bot#6984 .`,'https://www.twitch.tv/v5bz');
   console.log('---------------');
   console.log(' Bot Is Online')
@@ -196,5 +195,34 @@ if (message.content.startsWith("$ban")) {
 };
 });
 client.login("NDY4OTc4NTUxNzEwODEwMTEy.DjEOiQ.rMRu7xZVmJZPY5LbivXmqD_h74I");
+
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+  client.user.setGame(`on 15 servers | help$ | 5rbash bot#6984 .`,'https://www.twitch.tv/v5bz');
+  console.log('---------------');
+  console.log(' Bot Is Online')
+  console.log('---------------')
 
 client.login(process.env.BOT_TOKEN);
