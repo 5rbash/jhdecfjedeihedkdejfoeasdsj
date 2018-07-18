6124de8cdef17caea5fe96904fcacac69f2b2e74
5rbash <41377783+5rbash@users.noreply.github.com>
2018-07-18 17:52:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,20 +5,7 @@ const prefix = '.'
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
-  console.log('client.on('message', message => {
-if (message.content.startsWith("$kick")) {
-        if(!message.member.hasPermission('KICK_MEMBERS')) return message.reply('⚠ ماعندك الصلاحيات');
-    var mention = message.mentions.members.first();
-    if(!mention) return message.channel.send("يجب منشن العضو");
-
-    mention.kick("By: " + message.author.tag);
-
-       
-         return message.reply     
-   ("تم أعطاء الكيك بنجاح "    );
-  
-};
-});')
+  console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
   console.log(`[Start] ${new Date()}`);
@@ -42,6 +29,20 @@ if (message.content.startsWith("$kick")) {
 });
 
 
+client.on('message', message => {
+if (message.content.startsWith("$kick")) {
+        if(!message.member.hasPermission('KICK_MEMBERS')) return message.reply('⚠ ماعندك الصلاحيات');
+    var mention = message.mentions.members.first();
+    if(!mention) return message.channel.send("يجب منشن العضو");
+
+    mention.kick("By: " + message.author.tag);
+
+       
+         return message.reply     
+   ("تم أعطاء الكيك بنجاح "    );
+  
+};
+});
 
 
 client.login(process.env.BOT_TOKEN);
