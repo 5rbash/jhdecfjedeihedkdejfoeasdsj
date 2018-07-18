e6ec5d6bc4991c71d1118668a970dba2ffc54fd5
5rbash <41377783+5rbash@users.noreply.github.com>
2018-07-18 17:48:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,33 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
   console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('});
+if (message.content === '$help') {
+              var embed  = new Discord.RichEmbed()
+                .addField("**LINKS**" ,":no_entry_sign: تم اضافه منع الروابط :no_entry_sign: ")
+                .addField("broadcast (bc)" ,"**الاستخدام:** ``$broadcast <الرساله> , $bc <الرساله>``")
+                .addField("**BAN**" ,"**الاستخدام:** ``$ban <المستخدم>``")
+                .addField("**KICK**" ,"**الاستخدام:** ``$kick <المستخدم> ``")
+                .addField("**ِAVATAR**" ,"**الاستخدام:** ``$avatar``")
+                .addField("**INFO**", "**الأستخدام :** ``$info``")
+                .addField("**SAY**" ,"**الاستخدام:** ``$say <الرساله>``")
+                .addField("**ID**" ,"**ال��ستخدام:** ``$id``")
+                .addField("**SERVER**" ,"**الاستخدام:** ``$server``")
+                .addField("**INVITE**" ,"**الاستخدام:** ``$invite <لأضافه البوت لأى سيرفر>``")
+                .addField("**SUPPORT**" ,"**الاستخدام:** ``$suppport <سيرفر دعم الفنى>``")
+                .addField("**QA**" ,"**الاستخدام:** ``$qa <السؤال>``  ")
+                .addField("**CLEAR**" ,"**الاستخدام:** ``$clear <العدد>``")
+                .addField("**PING**", "**الأستخدام:** ``$ping``")
+                .addField("**SERVERNAME**", "**الأستخدام:** ``$servername``")
+                .addField("**CUT_TWEET**", "**الاستخدام** ``$ct``")
+                .addField("**TWEET**", "**الاستخدام** ``$tweet <الرساله>``")
+                .addField("**NO INVITE LINKE**","تم اضافة خاصية منع الانفيتات ")
+                .addField("**LOGIN**" , " تم اضافة خاصية التفعيل لطلب تشغيلها في السيرفر كلم المبيرمجين ")
+                .setColor('RANDOM')
+.setColor('RANDOM')
+  message.author.sendEmbed(embed);
+    }
+});')
   console.log('')
   console.log('╔[════════════════════════════════════]╗');
   console.log(`Logged in as * [ " ${client.user.username} " ]`);
