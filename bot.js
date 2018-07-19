597167d01b59f457618437b53b6bb6855f78f32c
5rbash <41377783+5rbash@users.noreply.github.com>
2018-07-19 15:26:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -193,4 +193,57 @@ if (message.content.startsWith("$ban")) {
   
 };
 });
+
+client.on('message', message => {
+if(message.content.startsWith(prefix +'sug')) {
+if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+      const A8tra7Room = message.guild.channels.find("name", "اسم الروم")
+      if(!message.channel.guild) return message.reply(`هذا الأمر فقط ل السيرفرات :x:`);
+   let a8tra7 = message.content.split(' ').slice(1).join(' ')
+   var m8tr7 = message.author.id
+if(!message.guild.channels.find("name","اسم الروم")) return message.channel.send('انشء روم باسم [اسم الروم]')
+var Eror = new Discord.RichEmbed()
+   .setTimestamp()
+   .setDescription(`الرجاء كتابت إقتراحك بعد الأمر `)
+   if(!a8tra7.join(" ")) return message.channel.send(Eror).then(message => {message.delete(50000)});
+   var ThxForSug = new Discord.RichEmbed()
+   .setTitle(`:white_check_mark: Success!`)
+   .setTimestamp()
+   .setDescription(`شكراً على اقتراحك !`)
+.setDescription(`**Your Suggestion** : ${a8tra7}`)
+   var Sure = new Discord.RichEmbed()
+   .setTimestamp()
+   .setTitle(`هل انت متأكد من ارسال الاقتراح؟ معك دقيقه قبل الالغاء`)
+.setDescription(`Suggestion : **${a8tra7}**`)
+		 .setFooter('DK SUPPORT' , client.user.avatarURL)
+message.channel.sendEmbed(Sure).then(msg => {
+    msg.react('❎')
+.then(() => msg.react('���'))
+
+let YesFilter = (reaction, user) => reaction.emoji.name === '✅'  && user.id === message.author.id;
+let NoFilter = (reaction, user) => reaction.emoji.name === '❎' && user.id === message.author.id;
+
+let Yes = msg.createReactionCollector(YesFilter, { time: 60000 });
+let No = msg.createReactionCollector(NoFilter, { time: 60000 });
+
+Yes.on("collect", r => {
+   var ala8tra7 = new Discord.RichEmbed()
+   .setTimestamp()
+   .setColor('RANDOM')
+   .setThumbnail(message.author.avatarURL)
+   .setTitle(`New Suggestion :bell:`)
+   .setDescription(`From : __<@${m8tr7}>__\n\nSuggestion: **${a8tra7}**`)
+   .setFooter(`${message.author.username}#${message.author.discriminator}`)
+   A8tra7Room.send(ala8tra7)
+   message.channel.sendEmbed(ThxForSug).then(message => {message.delete(6000)})
+msg.delete();
+})
+No.on("collect", r => {
+message.channel.send('تم الغاء اقتراحك بنجاح :white_check_mark: ').then(message => {message.delete(4000)})
+msg.delete();
+})
+})
+}
+}); 
+
 client.login("NDY4OTc4NTUxNzEwODEwMTEy.DjCa_Q.dvqOZsZxab7ztE2h71vRMqb_IBM");
