b1691485cc1d026a83cce6e12a6647ba2812c999
5rbash <41377783+5rbash@users.noreply.github.com>
2018-07-19 11:13:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,24 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = '.'
-
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
   client.user.setGame(`on 15 servers | help$ | 5rbash bot#6984 .`,'https://www.twitch.tv/v5bz');
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
+  console.log('---------------');
   console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+  console.log('---------------')
+});
+client.on('message', message => {
+     if (message.content === ".servers") {
+     let embed = new Discord.RichEmbed()
+  .setColor("#0000FF")
+  .addField("**Server: **" , client.guilds.size)
+  message.channel.sendEmbed(embed);
+    }
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
 if (message.content === '$help') {
               var embed  = new Discord.RichEmbed()
                 .addField("**LINKS**" ,":no_entry_sign: تم اضافه منع الروابط :no_entry_sign: ")
@@ -199,4 +192,3 @@ if (message.content.startsWith("$ban")) {
 };
 });
 client.login("NDY4OTc4NTUxNzEwODEwMTEy.DjHY-Q.0hB0xiW28VlBpYxzLfxNnHQhXvc");
-client.login(process.env.BOT_TOKEN);
