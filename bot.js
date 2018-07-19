d40de05bb4c125737237f88d5a6586e97317661e
5rbash <41377783+5rbash@users.noreply.github.com>
2018-07-19 19:48:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -227,42 +227,25 @@ client.on('message', message => {
    }
 });
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const YTDL = require("ytdl-core");
-const prefix = '$';
-function play(connection, message) {
-    var server = servers[message.guild.id];
-    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
-    server.queue.shift();
-    server.dispatcher.on("end", function() {
-        if(server.queue[0]) play(connection);
-        else connection.disconnect();
-    })
-}
-var servers = {};
 client.on('message' , async (message) => {
-       if(message.content.startsWith(prefix + "$play")) {
-              let args = message.content.split(" ").slice(1);
-    //play
-    if (!args[0]) {
-         message.channel.send("Please specify a link");
-         return
-    }
-    if(!message.member.voiceChannel) {
-        message.channel.sned("I think it may work better if you are in a voice channel!");
-    }
-    if(!servers[message.guild.id]) servers[message.guild.id] = {
-        queue: []
-    }
-    var server = servers[message.guild.id];
-    server.queue.push(args[0]);
-    message.channel.send("Your song of choice is on the queue.` ")
-    if(!message.member.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
-        play(connection, message);
-    })
-}
-
+       if(message.content.startsWith(`<@${client.user.id}>`)) {
+              
+ let responses = [
+        'كيف يمكن اساعدك',
+        'مرحبا',
+        'لا تزعجني',
+        'ايش تبي ',
+        'هلا',
+        'كيفك',
+        '?',
+        'جرب *help'
+    ]
+    
+    // Fetch a random item from the array
+    let fetched = responses[Math.floor(Math.random() * responses.length)];
+   message.reply(fetched)
+       }
+  
 });
 
 client.login("NDY4OTc4NTUxNzEwODEwMTEy.DjCa_Q.dvqOZsZxab7ztE2h71vRMqb_IBM");
