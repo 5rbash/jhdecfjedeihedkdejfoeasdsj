4a5eaf0154ee33b893a7cb0ffef9d3ccb77040f5
5rbash <41377783+5rbash@users.noreply.github.com>
2018-07-21 16:03:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -333,7 +333,7 @@ client.on('message', message => {
 });
 
 const discord = require('discord.js');
-const client = new Discord.Client();
+const Client = new Discord.Client();
 const ytdl = require('ytdl-core');
 const request = require('request');
 const fs = require('fs');
